08ff21aed9e4ed05ace6b7723d3f1ed16b368077
PrinOrange <prinorange@outlook.com>
2024-09-11 21:46:04 +0800

    [fix] fix some faults in link-directing

--- components/utils/Footer.tsx
+++ components/utils/Footer.tsx
@@ -1,4 +1,10 @@
-import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 import { CopyrightAnnouncement, RSSFeedURL } from "@/consts/consts";
 import { Config } from "@/data/config";
 import Link from "next/link";
@@ -20,7 +26,11 @@ export const Footer = () => {
     >
       <footer className="my-5 flex flex-col justify-center py-2 text-sm">
         <div className="my-2 flex flex-wrap justify-center space-x-3 text-center text-gray-500 underline dark:text-gray-400">
-          <Link href="https://github.com/PrinOrange/nextjs-lexical-blog" title="Sponsor me for my works.">
+          <Link
+            href="https://github.com/PrinOrange/nextjs-lexical-blog"
+            title="The source code of this blog site template."
+            target="_blank"
+          >
             {"Source Code"}
           </Link>
           <Link href="/sponsor" title="Sponsor me for my works.">
@@ -37,7 +47,9 @@ export const Footer = () => {
             </DialogTrigger>
           )}
         </div>
-        <div className="mx-auto px-3 text-center font-bold">{CopyrightAnnouncement}</div>
+        <div className="mx-auto px-3 text-center font-bold">
+          {CopyrightAnnouncement}
+        </div>
         <DialogContent className="sm:max-w-md">
           <DialogHeader>
             <DialogTitle className="flex">
@@ -49,8 +61,9 @@ export const Footer = () => {
           <div>
             <div className="my-2 w-full text-sm">
               <div>
-                <b>NOTE: </b>Some RSS Feed Reader may has deficient in rendering SVG formulations, graphs. Such as the
-                Inoreader, Feedly. If it happens, please read the origin web page for better experience.
+                <b>NOTE: </b>Some RSS Feed Reader may has deficient in rendering
+                SVG formulations, graphs. Such as the Inoreader, Feedly. If it
+                happens, please read the origin web page for better experience.
               </div>
             </div>
             <Separator />
@@ -63,12 +76,18 @@ export const Footer = () => {
                 text={RSSFeedURL}
               >
                 <Button
-                  className={`my-auto ml-3 ${isCopied && "bg-green-500 hover:bg-green-500"}`}
+                  className={`my-auto ml-3 ${
+                    isCopied && "bg-green-500 hover:bg-green-500"
+                  }`}
                   size="sm"
                   type="submit"
                 >
                   <span className="sr-only">{"Copy"}</span>
-                  {isCopied ? <FaCheck className="h-4 w-4" /> : <FaCopy className="h-4 w-4" />}
+                  {isCopied ? (
+                    <FaCheck className="h-4 w-4" />
+                  ) : (
+                    <FaCopy className="h-4 w-4" />
+                  )}
                 </Button>
               </CopyToClipboard>
             </div>
